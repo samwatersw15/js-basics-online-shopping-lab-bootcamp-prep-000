c0d26dad56cadcb18fb6bdd38a7926cf40ddc15c
Sam Waters <samwatersw15@gmail.com>
2019-04-28 11:19:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function viewCart() {
  for (let i = 0; i<cart.length; i++) {
    cartArray.push(` you have ${cart[i]['itemName']} at $${cart[i]['itemPrice']}`)
  }
- let b = cartArray.length
- return 'In your cart,'.concat(cartArray.slice(0,b-1))+'and'.concat(cartArray.slice(b))+'.'
+ 
+ return 'In your cart,'.concat(cartArray.slice(0,cartArray.length-1))+'and'.concat(cartArray.slice(cartArray.length-1))+'.'
 }
 
 function total() {
