f17ad31a7a4ffae52ec05816e0298fa441a717aa
Sam Waters <samwatersw15@gmail.com>
2019-04-28 11:50:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,7 +65,8 @@ function placeOrder(cardNumber) {
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
   else {
+    let t = total()
     cart = []
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${t}, which will be charged to the card ${cardNumber}.`
   }
 }
