828e0b31c49fecb51d98ee395db5f702e4d4c799
Sam Waters <samwatersw15@gmail.com>
2019-04-28 11:31:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i<cart.length; i++) {
     if (cart[i]['itemName'] == item) {
-      cart.splice(i,1)
+      return cart.splice(i,1)
     } else {
       return 'That item is not in your cart'
     }
