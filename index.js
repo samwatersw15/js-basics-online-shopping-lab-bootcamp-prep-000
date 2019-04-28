bcdb90c0a1fca4dc7431956631849c48247ff97b
Sam Waters <samwatersw15@gmail.com>
2019-04-28 11:42:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber === 0) {
+  if (cardNumber < 1) {
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
   else {
