0537ed66cd1fe5c720a48d94098b99c89dcec239
Sam Waters <samwatersw15@gmail.com>
2019-04-28 11:33:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i<cart.length; i++) {
     if (cart[i]['itemName'] == item) {
-      console.log(item)
+      return cart.splice(i,1)
     } else {
-      console.log('That item is not in your cart')
+      return 'That item is not in your cart'
     }
   }
 }
